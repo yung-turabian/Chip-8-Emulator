8a1fa14c5c3110182f0424f0b975bc52eafa581b
Henry Wandover <henrywandover@gmail.com>
2025-06-11 12:30:26 -0400

    Removed junk files

--- src/c8/c8_functions.d
+++
@@ -1,9 +0,0 @@
-src/c8/c8_functions.o: src/c8/c8_functions.c src/c8/c8_functions.h \
- src/c8/c8_system.h src/c8/c8_shared.h src/c8/../log.h \
- src/c8/c8_display.h src/c8/c8.h
-src/c8/c8_functions.h:
-src/c8/c8_system.h:
-src/c8/c8_shared.h:
-src/c8/../log.h:
-src/c8/c8_display.h:
-src/c8/c8.h:
